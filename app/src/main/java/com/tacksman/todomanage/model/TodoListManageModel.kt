ba8590f968c23052900d277f9f7cf3a06ebe9294
Taku Kuwabara <tacksman666@gmail.com>
2017-12-18 08:22:15 +0900

    TodoManageActivity用のモデルを作成

--- app/src/main/java/com/tacksman/todomanage/model/TodoListManageModel.kt
+++ app/src/main/java/com/tacksman/todomanage/model/TodoListManageModel.kt
@@ -1,7 +1,48 @@
 package com.tacksman.todomanage.model
 
-/**
- * Created by tacksman on 2017/12/17.
- */
-class TodoListManageModel {
+import android.os.Parcel
+import android.os.Parcelable
+import android.util.Log
+import com.tacksman.todomanage.entity.Todo
+import com.tacksman.todomanage.infrastructure.TodoManageRepository
+
+class TodoListManageModel() : Parcelable {
+
+    val repository = TodoManageRepository()
+
+    var todoList = emptyList<Todo>()
+
+    constructor(parcel: Parcel) : this() {
+        todoList = parcel.createTypedArrayList(Todo)
+    }
+
+    suspend fun todoList(): List<Todo> {
+        val fetchedTodoList = repository.fetchList()
+//        todoList.addAll(repository.fetchList())
+        Log.d(this::class.java.simpleName, fetchedTodoList.toString())
+        return fetchedTodoList
+    }
+
+    suspend fun addTodo(todo: Todo): String {
+        return repository.add(todo)
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeTypedList(todoList)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<TodoListManageModel> {
+        override fun createFromParcel(parcel: Parcel): TodoListManageModel {
+            return TodoListManageModel(parcel)
+        }
+
+        override fun newArray(size: Int): Array<TodoListManageModel?> {
+            return arrayOfNulls(size)
+        }
+    }
+
 }
